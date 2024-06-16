f9d91442066a5577cf3d963eb31e47807a75639f
ediiizte <144380106+ediiizte@users.noreply.github.com>
2024-06-16 14:47:18 +0100

    Add files via upload

--- chessbot/board_chess1.3.py
+++ chessbot/board_chess1.3.py
@@ -91,22 +91,22 @@ class Position:
         match ID2:
             case "pawn":
                 piece1 = Pawn(x,y,self,moved,scrn_size,colour,ID)
-        piece1.setup_img_coord()
+        piece1.update_img_coord()
         self.pieces.append(piece1)
-    def get_pieces_img(self):
-        for piece in self.pieces:
-            piece.image()
+        piece1.image()
+
     def display_pieces(self):
         for piece in self.pieces:
             piece.draw()
     def get_closest_piece(self):
         mouse_coord = pygame.mouse.get_pos()
+        min_dist = []
         for piece in self.pieces:
             coord2 = (piece.img_x,piece.img_y)
-            print(coord2)
-            print(mouse_coord)
-            if self.distance(mouse_coord,coord2) < self.scrn_size/10:
-                return piece
+            dist = self.distance(mouse_coord,coord2)
+            min_dist.append(dist)
+        min_piece_index = min_dist.index(min(min_dist))
+        return self.pieces[min_piece_index]
         return None
     def distance(self,coord1,coord2):
         return ((coord1[0]-coord2[0])**2+(coord1[1]-coord2[1])**2)**0.5
@@ -116,7 +116,6 @@ class Position:
         if self.check_for_clicked_piece() == None and self.mouse_click() == True:
             piece_clicked = self.get_closest_piece()
             if piece_clicked != None:
-                print(piece_clicked)
                 piece_clicked.clicked = True
                 piece_clicked.click()
     def check_for_clicked_piece(self):
@@ -128,10 +127,7 @@ class Position:
         piece = self.check_for_clicked_piece()
         if piece != None:
             piece.click()
-                
-            
 
-        
             
 class Piece:
     def __init__(self,x,y,position,moved,scrn_size,colour,ID):
@@ -141,20 +137,24 @@ class Piece:
         self.img = ""
         self.x = x
         self.y = y
+        self.og_xy = (self.x,self.y)
         self.img_x = 0
         self.img_y = 0
         self.position = position
         self.moved = moved
         self.moves = []
         self.clicked = False
-    def setup_img_coord(self):
+    def update_img_coord(self):
         self.img_x = (self.y)*self.scrn_size/8
         self.img_y = (self.x)*self.scrn_size/8
     def image(self):
         self.img = pygame.image.load(f"{self.colour}_{self.ID}.png")
         self.img = pygame.transform.scale(self.img,(self.scrn_size/8,self.scrn_size/8))
     def draw(self):
-        screen.blit(self.img,(self.img_x,self.img_y))
+        try:
+            screen.blit(self.img,(self.img_x,self.img_y))
+        except:
+            TypeError
     def index_check(self,move):
         if -1 < self.x + move[0] < 8 and -1 < self.y + move[1] < 8:
             return True
@@ -163,7 +163,6 @@ class Piece:
         if self.position.empty(square) == False and self.position.capital(square) == self.colour:
             self.moves.pop(move)
                 
-        print(square)
     def quick_move_check(self):
         for move in self.moves:
             if self.index_check(move) == False or self.same_piece_check(move) == True:
@@ -171,10 +170,8 @@ class Piece:
     def click(self):
         if self.position.mouse_click() == True:
             self.img_x,self.img_y = pygame.mouse.get_pos()[0]-0.5*self.scrn_size/8,pygame.mouse.get_pos()[1]-0.5*self.scrn_size/8
-            print(self.img_x,self.img_y)
         else:
             self.clicked = False
-            self.update_coord()
             self.img_x = self.find_closest_square(self.img_x)
             self.img_y = self.find_closest_square(self.img_y)
             self.take_piece()
@@ -194,11 +191,26 @@ class Piece:
             coord += (100-value)
         return coord
     def update_coord(self):
-        self.x,self.y = self.img_x*8/self.scrn_size,self.img_y*8/self.scrn_size
+        print(self.x,self.y)
+        self.x,self.y = int(self.img_y*8/self.scrn_size),int(self.img_x*8/self.scrn_size)
+        if self.x > 7 or self.x < 0 or 0 > self.y or self.y > 7:
+            print(1)
+            print("x",self.x,"y",self.y)
+            self.x,self.y = self.og_xy[0],self.og_xy[1]
+            print(self.x,self.y)
+            self.update_img_coord()
+        else:
+            self.og_xy = (self.x,self.y)
+        self.promote()
     def take_piece(self):
         for piece in self.position.pieces:
             if (piece.img_x,piece.img_y) == (self.img_x,self.img_y) and piece.colour != self.colour:
                 self.position.pieces.remove(piece)
+    def promote(self):
+        if self.ID == "pawn":
+            if self.colour == "white" and self.x == 0 or self.colour == "black" and self.x == 7:
+                self.position.create_piece(self.x,self.y,self.moved,self.scrn_size,self.colour,"queen")
+                self.position.pieces.remove(self)
             
 class Rook(Piece):
     x_moves = [[-7, 0], [-6, 0], [-5, 0], [-4, 0], [-3, 0], [-2, 0], [-1, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0]]
@@ -258,11 +270,10 @@ start_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
 current = Position(start_fen,800)
 current.get_board()
 current.setup_board_pieces()
-current.get_pieces_img()
 current.create_group(size[0])
-knight = Knight(2,2,current,False,800,"white","knight")
-knight.same_piece_check([1,2])
-knight.image()
+# knight = Knight(2,2,current,False,800,"white","knight")
+# knight.same_piece_check([1,2])
+# knight.image()
 # class of chess board drawing
  
 # Loop until the user clicks the close button.
@@ -292,7 +303,7 @@ while not done:
  
     # --- Drawing code should go here
     current.draw_squares(screen)
-    knight.draw()
+#     knight.draw()
     current.display_pieces()
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
